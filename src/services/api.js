92cbb174c1829e319c525cbe34b3c66d94d923ac
Gian <gianszabernardo@gmail.com>
2020-05-12 18:53:14 -0300

    Add temporary error boundaries

--- src/services/api.js
+++ src/services/api.js
@@ -9,36 +9,56 @@ class APIService {
   async getAllEntities() {
     try {
       const allEntities = await this.apiInstance.get();
-      return allEntities.data;
-    } catch (err) {}
+      if (allEntities) return allEntities.data;
+
+      return "Error.";
+    } catch (err) {
+      return err.response;
+    }
   }
 
   async getEntity(entityId) {
     try {
       const singleEntity = await this.apiInstance.get("/" + entityId);
-      return singleEntity.data;
-    } catch (err) {}
+      if (singleEntity) return singleEntity.data;
+
+      return "Error.";
+    } catch (err) {
+      return err.response;
+    }
   }
 
   async createEntity(entityInfo) {
     try {
       const entity = await this.apiInstance.post("/", entityInfo);
-      return entity;
-    } catch (err) {}
+      if (entity) return entity;
+
+      return "Error.";
+    } catch (err) {
+      return err.response;
+    }
   }
 
   async updateEntity(entityId, entityInfo) {
     try {
       const entity = await this.apiInstance.put("/" + entityId, ...entityInfo);
-      return entity;
-    } catch (err) {}
+      if (entity) return entity;
+
+      return "Error.";
+    } catch (err) {
+      return err.response;
+    }
   }
 
   async deleteEntity(entityId) {
     try {
       const entity = await this.apiInstance.delete("/" + entityId);
-      return entity;
-    } catch (err) {}
+      if (entity) return entity;
+
+      return "Error.";
+    } catch (err) {
+      return err.response;
+    }
   }
 }
 
