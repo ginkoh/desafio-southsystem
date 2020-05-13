a4d56d6ae32e7d617ebc1daa5c389a327012f9db
Gian <gianszabernardo@gmail.com>
2020-05-13 18:17:58 -0300

    Create a loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,18 @@
+// React.
+import React from "react";
+
+function Loading() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      ...
+    </div>
+  );
+}
+
+export default Loading;
