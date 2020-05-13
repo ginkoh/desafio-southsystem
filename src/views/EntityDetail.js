656c004f22b1a3179807e59db687ea3766a14914
Gian <gianszabernardo@gmail.com>
2020-05-13 18:18:57 -0300

    Finish the entity detail

--- src/views/EntityDetail.js
+++ src/views/EntityDetail.js
@@ -1,21 +1,54 @@
 // React.
 import React from "react";
+import { useParams, Link } from "react-router-dom/cjs/react-router-dom";
+
+// Hooks.
+import useEntitiesInformation from "../hooks/useEntitiesInformation";
 
 // Components.
 import EntityCard from "../components/EntityCard";
-import useEntitiesInformation from "../hooks/useEntitiesInformation";
-import { useParams } from "react-router-dom/cjs/react-router-dom";
+import Loading from "../components/Loading";
+
+// Utils.
+import { entityServiceInfo } from "../constants/services";
+
+// Third party.
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  margin: 5px;
+  border-radius: 3px;
+  padding: 10px;
+  background: #ececec;
+  outline: none;
+  text-decoration: none;
+  color: #000;
+`;
 
 function EntityDetail() {
   const { entityId } = useParams();
   const { entity, loading } = useEntitiesInformation(entityId);
 
-  if (loading) return <h1>...</h1>;
+  if (loading) return <Loading></Loading>;
+
+  const entityDetailLinkPrefix = `/${entityServiceInfo.pluralPrefix}/detail/`;
+
+  const previousEntity = parseInt(entityId, 10) - 1 || 1;
+  const nextEntity = parseInt(entityId, 10) + 1;
 
   return (
-    <div>
-      <h1>Entity Detail</h1>
-      <EntityCard {...entity}></EntityCard>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div>
+        <EntityCard {...entity} entityId={entityId}></EntityCard>
+      </div>
+      <div style={{ alignSelf: "flex-end", margin: "100px" }}>
+        <StyledLink to={entityDetailLinkPrefix + previousEntity.toString()}>
+          Anterior
+        </StyledLink>
+        <StyledLink to={entityDetailLinkPrefix + nextEntity.toString()}>
+          Proxima
+        </StyledLink>
+      </div>
     </div>
   );
 }
