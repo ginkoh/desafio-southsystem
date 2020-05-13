ccd8b80db68ffa2a9d14294cd6f32c51958c26bf
Gian <gianszabernardo@gmail.com>
2020-05-12 22:55:01 -0300

    Add local storage basic logic into the @useAuthentication hook

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,20 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useCallback } from "react";
 import { setAuthenticated, setUnauthenticated } from "../ducks/authentication";
+import { DEFAULT_USERNAME, DEFAULT_PASSWORD } from "../constants";
 
 function useAuthentication() {
   const dispatch = useDispatch();
-  const authenticated = useSelector(
+  let authenticated = useSelector(
     (state) => state.authentication.authenticated
   );
 
+  if (localStorage.getItem("challenge-auth")) authenticated = true;
+
   const authenticate = useCallback(() => {
+    localStorage.setItem("challenge-auth", "authenticated");
+
     dispatch(setAuthenticated());
-  }, []);
+  }, [dispatch]);
 
   const unauthenticate = useCallback(() => {
+    localStorage.removeItem("challenge-auth");
     dispatch(setUnauthenticated());
-  }, []);
+  }, [dispatch]);
 
   return {
     authenticated,
