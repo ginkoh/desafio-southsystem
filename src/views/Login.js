76ffa205cf99ce62d626aa10d2da261bba34f49d
Gian <gianszabernardo@gmail.com>
2020-05-12 22:53:58 -0300

    Create the login form base

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,11 +1,50 @@
 // React.
 import React from "react";
+import { Redirect } from "react-router-dom";
+
+// Hooks.
+import useAuthentication from "../hooks/useAuthentication";
+
+// Third party.
+import { useFormik } from "formik";
+import { loginUser, DEFAULT_USERNAME, DEFAULT_PASSWORD } from "../constants";
 
 function LoginPage() {
-  return (
-    <div>
-      <h1>LoginPage</h1>
-    </div>
+  const { authenticated, authenticate } = useAuthentication();
+  const formik = useFormik({
+    initialValues: {
+      username: DEFAULT_USERNAME,
+      password: DEFAULT_PASSWORD,
+    },
+    onSubmit: ({ username, password }) => {
+      if (!authenticated && loginUser(username, password)) {
+        authenticate();
+      }
+    },
+  });
+
+  return authenticated ? (
+    <Redirect to="/"></Redirect>
+  ) : (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="username"></label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        onChange={formik.handleChange}
+        value={formik.values.username}
+      />
+      <label htmlFor="password"></label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+      <button type="submit">Entrar</button>
+    </form>
   );
 }
 
