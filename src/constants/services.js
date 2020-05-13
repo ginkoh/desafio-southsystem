6dec990ef96e153cd873f7c352ff3c990c9989e2
Gian <gianszabernardo@gmail.com>
2020-05-13 18:19:35 -0300

    Add more constants to the services

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,4 +1,9 @@
 export const entityServiceInfo = {
   apiUrl: "http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/",
   prefix: "dragon",
+  pluralPrefix: "dragons",
 };
+
+export const formattedPrefixName =
+  entityServiceInfo.pluralPrefix[0].toUpperCase() +
+  entityServiceInfo.pluralPrefix.slice(1, -1);
