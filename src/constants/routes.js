8175a1301541175bc329b134a23d8010d18ab364
Gian <gianszabernardo@gmail.com>
2020-05-12 22:56:09 -0300

    Separate and make the routes reusable by passing the entity prefix

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -6,7 +6,15 @@ import EntityDetail from "../views/EntityDetail";
 import EntityCreation from "../views/EntityCreation";
 import LoginPage from "../views/Login";
 
+// Utils.
+import { entityServiceInfo } from "./services";
+
+export const routesPrefix = entityServiceInfo.prefix + "s";
+
 const entitiesRoutes = [
+  /**
+   * Root route.
+   */
   {
     path: "/",
     component: () => {
@@ -18,30 +26,54 @@ const entitiesRoutes = [
     },
     exact: true,
     protected: true,
-  },
-  {
-    path: "/entities/all/",
-    component: EntityList,
-    protected: true,
-  },
-  {
-    path: "/entities/detail/:entityId",
-    component: EntityDetail,
-    protected: true,
-  },
-  {
-    path: "/entities/create/",
-    component: EntityCreation,
-    protected: true,
+    pageTitle: "Dashboard",
   },
 
+  /**
+   * Content routes.
+   */
+  {
+    path: `/${routesPrefix}/all`,
+    component: EntityList,
+    protected: true,
+    pageTitle: "Lista de ",
+  },
+  {
+    path: `/${routesPrefix}/detail/:entityId"`,
+    component: EntityDetail,
+    protected: true,
+    pageTitle: "Detalhes -",
+  },
+  {
+    path: `${routesPrefix}/create`,
+    component: EntityCreation,
+    protected: true,
+    pageTitle: "Criar ",
+  },
+];
+
+const globalRoutes = [
+  /**
+   * Login route.
+   */
   {
     path: "/login",
     component: LoginPage,
     protected: false,
+    pageTitle: "Login",
+  },
+
+  /**
+   * 404 route.
+   */
+  {
+    path: null,
+    component: () => {
+      return <h1>404 - Page not found :(</h1>;
+    },
+    protected: false,
+    pageTitle: "Erro",
   },
 ];
-
-const globalRoutes = [];
 
 export const routes = [...entitiesRoutes, ...globalRoutes];
