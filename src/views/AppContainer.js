157cedc6eeb1109c287c541a6f1f89e8ec97458a
Gian <gianszabernardo@gmail.com>
2020-05-13 18:19:27 -0300

    Update the app container to use fontawesome and an application frame

--- src/views/AppContainer.js
+++ src/views/AppContainer.js
@@ -2,20 +2,45 @@
 import React, { Fragment } from "react";
 import { Switch, Router } from "react-router-dom";
 
-// Routes.
-import { routes } from "../constants/routes";
+// Components.
 import CustomRoute from "../components/CustomRoutes";
+
+// Utils.
+import { routes } from "../constants/routes";
 import history from "../utils/history";
 
+// Third party.
+import styled from 'styled-components';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+library.add(fab, fas, far);
+
+const ApplicationFrame = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  height: 100%;
+
+  @media only screen and (max-width: 700px) {
+    flex-direction: column;
+  }
+`;
+
 function AppContainer() {
+
   return (
     <Router history={history}>
       <Fragment>
-        <Switch>
-          {routes.map((routeProps, idx) => {
-            return <CustomRoute {...routeProps} key={idx} />;
-          })}
-        </Switch>
+        <ApplicationFrame>
+          <Switch>
+            {routes.map((routeProps, idx) => {
+              return <CustomRoute {...routeProps} key={idx} />;
+            })}
+          </Switch>
+        </ApplicationFrame>
       </Fragment>
     </Router>
   );
