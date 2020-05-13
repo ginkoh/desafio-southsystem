af469560224df0e55ced40b13fd4ae1e75528e9b
Gian <gianszabernardo@gmail.com>
2020-05-13 18:20:28 -0300

    Update the entity list component to show a list of entities

--- src/views/EntityList.js
+++ src/views/EntityList.js
@@ -1,88 +1,42 @@
 // React.
-import React, { useState, useMemo } from "react";
+import React from "react";
+
+// Components.
+import Loading from "../components/Loading";
+import EntityCard from "../components/EntityCard";
+
+// Hooks.
 import useEntitiesInformation from "../hooks/useEntitiesInformation";
+
+// Utils.
+import { entityServiceInfo, formattedPrefixName } from "../constants/services";
+import history from "../utils/history";
 
 function EntityList() {
   const { entities, loading } = useEntitiesInformation();
-  const [editingElement, setEditingElement] = useState({
-    isEditing: false,
-    elementId: null,
-  });
 
-  const fields = useMemo(
-    () => ({
-      name: "Nome",
-      type: "Tipo",
-      createdAt: "Data de criaçao",
-    }),
-    []
-  );
-
-  if (loading) return <h1>...</h1>;
+  if (loading) return <Loading />;
 
   // Sort the entities by alphabetic order.
   const sortedEntities = entities.sort((a, b) => (a.name > b.name ? 1 : -1));
 
   return (
-    <div>
-      <h1>Entity List</h1>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        {sortedEntities.map((entity, idx) => {
-          return (
-            <div
-              key={idx}
-              style={{
-                flex: 1,
-                border: "1px solid red",
-                margin: "5px",
-                minHeight: "200px",
-                maxHeight: "200px",
-              }}
-            >
-              {Object.keys(fields).map((key, i) => {
-                const currentField = fields[key];
-
-                return (
-                  <div key={i}>
-                    <span>
-                      {currentField}: {}
-                    </span>
-                    {editingElement.isEditing &&
-                    editingElement.elementId === entity.id + "-" + key ? (
-                      <input
-                        defaultValue={entity[key]}
-                        onBlur={() =>
-                          setEditingElement({
-                            isEditing: false,
-                            elementId: null,
-                          })
-                        }
-                        onChange={(e) => {
-                          // e.target.value
-                        }}
-                      ></input>
-                    ) : (
-                      <span
-                        onClick={() =>
-                          setEditingElement({
-                            isEditing: true,
-                            elementId: entity.id + "-" + key,
-                          })
-                        }
-                      >
-                        {entity[key]}
-                      </span>
-                    )}
-                  </div>
-                );
-              })}
-              <div>
-                <img src={entity.imageUrl} alt="" />
-              </div>
-            </div>
-          );
-        })}
-      </div>
+    <div style={{ margin: "20px", overflowY: "auto" }}>
+      <h1>{`${formattedPrefixName} List`}</h1>
+      {sortedEntities.map((entity, idx) => {
+        return (
+          <div
+            key={idx}
+            onClick={() => {
+              history.push(
+                `/${entityServiceInfo.pluralPrefix}/detail/${entity.id}`
+              );
+            }}
+          >
+            <EntityCard isListPage={true} {...entity}></EntityCard>
+          </div>
+        );
+      })}
     </div>
   );
 }
