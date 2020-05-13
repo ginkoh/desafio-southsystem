0f8bacb38b59096482f149f68bc57eb99276696a
Gian <gianszabernardo@gmail.com>
2020-05-13 18:16:58 -0300

    Update custom routes

--- src/components/CustomRoutes.js
+++ src/components/CustomRoutes.js
@@ -1,25 +1,45 @@
 // React.
-import React from "react";
+import React, { Fragment } from "react";
 import { Route } from "react-router-dom";
+import { Redirect } from "react-router-dom/cjs/react-router-dom";
 
 // Hooks.
 import useAuthentication from "../hooks/useAuthentication";
 import usePageTitle from "../hooks/usePageTitle";
 
+// Components.
+import Sidebar from "../components/Sidebar";
+
+// Ducks.
+import { setActiveMenu } from "../ducks/sidebar";
+
 // Utils.
 import { entityServiceInfo } from "../constants/services";
-import { Redirect } from "react-router-dom/cjs/react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 export function AuthenticatedRoute(props) {
   const authenticatedRouteBehavior = {};
 
-  return <Route {...props} {...authenticatedRouteBehavior}></Route>;
+  const { activeMenu } = useSelector((state) => state.sidebar);
+  const dispatch = useDispatch();
+
+  return (
+    <Fragment>
+      {props.authentication.authenticated && (
+        <Sidebar
+          activeMenu={activeMenu}
+          setActiveMenu={(menuItem) => dispatch(setActiveMenu(menuItem))}
+        />
+      )}
+      <Route {...props} {...authenticatedRouteBehavior}></Route>
+    </Fragment>
+  );
 }
 
 export function UnauthenticatedRoute(props) {
   const unauthenticatedRouteBehavior = {};
 
-  return <Redirect to="/login"></Redirect>;
+  return <Redirect to="/login" {...unauthenticatedRouteBehavior}></Redirect>;
 }
 
 function CustomRoute(props) {
@@ -27,13 +47,18 @@ function CustomRoute(props) {
     props.pageTitle,
     props.exact || !props.path || props.path === "/login"
       ? ""
-      : entityServiceInfo.prefix + "s"
+      : entityServiceInfo.pluralPrefix
   );
 
   const authentication = useAuthentication();
 
   if (!props.protected || (props.protected && authentication.authenticated)) {
-    return <AuthenticatedRoute {...props}></AuthenticatedRoute>;
+    return (
+      <AuthenticatedRoute
+        {...props}
+        authentication={authentication}
+      ></AuthenticatedRoute>
+    );
   } else {
     return <UnauthenticatedRoute {...props}></UnauthenticatedRoute>;
   }
